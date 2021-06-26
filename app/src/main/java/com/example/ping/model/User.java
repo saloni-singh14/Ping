dd07c07c41bd655f748d31931525e7859b008c2a
Saloni <s.saloni1401@gmail.com>
2021-06-26 17:56:21 +0530

    Added all the demo app code. Facing issues with bugs.

---
+++ app/src/main/java/com/example/ping/model/User.java
@@ -0,0 +1,17 @@
+package com.example.ping.model;
+
+public class User {
+    private int agoraUid;
+
+    public User () {
+
+    }
+
+    public int getAgoraUid() {
+        return agoraUid;
+    }
+
+    public void setAgoraUid(int agoraUid) {
+        this.agoraUid = agoraUid;
+    }
+}
