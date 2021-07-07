f8b6927d8ecc34bee57578f8bb97c90fe7e8e6e8
Saloni <s.saloni1401@gmail.com>
2021-07-08 03:24:11 +0530

    Added Messaging functionality and reaction functionality. We can see past messaging history as well as timestamp of messages. Finished messaging functionality but there is a bug, which is causing the app to crash

--- app/src/main/java/com/example/ping/MessagesAdapter.java
+++ app/src/main/java/com/example/ping/MessagesAdapter.java
@@ -1,4 +1,345 @@
 package com.example.ping;
 
-public class MessagesAdapter {
+import android.app.AlertDialog;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.ping.databinding.DeleteDialogBinding;
+import com.example.ping.databinding.ItemReceiveBinding;
+import com.example.ping.databinding.ItemSentBinding;
+import com.github.pgreze.reactions.ReactionPopup;
+import com.github.pgreze.reactions.ReactionsConfig;
+import com.github.pgreze.reactions.ReactionsConfigBuilder;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+public class MessagesAdapter extends RecyclerView.Adapter {
+
+    Context context;
+    ArrayList<Message> messages;
+
+    final int ITEM_SENT = 1;
+    final int ITEM_RECEIVE = 2;
+
+    String senderRoom;
+    String receiverRoom;
+
+    public MessagesAdapter(Context context, ArrayList<Message> messages, String senderRoom, String receiverRoom) {
+        this.context = context;
+        this.messages = messages;
+        this.senderRoom = senderRoom;
+        this.receiverRoom = receiverRoom;
+    }
+
+
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        /// <summary>
+        /// Usually we inflate the layout file to bind it to the backend code.
+        ///Here, since there are two layout resources Item_sent and Item_receive, we use a
+        ///viewType variable for binding.
+        /// </summary>
+
+        if(viewType == ITEM_SENT) {
+            View view = LayoutInflater.from(context).inflate(R.layout.item_sent, parent, false);
+            return new SentViewHolder(view);
+        } else {
+            View view = LayoutInflater.from(context).inflate(R.layout.item_receive, parent, false);
+            return new ReceiverViewHolder(view);
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        Message message = messages.get(position);
+        ///If user ID is equal to the ID of the sender, then viewType is returned
+        /// as ITEM_SENT, else ITEM_RECEIVE
+        if(FirebaseAuth.getInstance().getUid().equals(message.getSenderId())) {
+            return ITEM_SENT;
+        } else {
+            return ITEM_RECEIVE;
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        Message message = messages.get(position);
+
+        int reactions[] = new int[]{
+                R.drawable.ic_fb_like,
+                R.drawable.ic_fb_love,
+                R.drawable.ic_fb_laugh,
+                R.drawable.ic_fb_wow,
+                R.drawable.ic_fb_sad,
+                R.drawable.ic_fb_angry
+        };
+
+        ReactionsConfig config = new ReactionsConfigBuilder(context)
+                .withReactions(reactions)
+                .build();
+        ///popup gives the option to react to messages
+        ReactionPopup popup = new ReactionPopup(context, config, (pos) -> {
+            if(holder.getClass() == SentViewHolder.class) {
+                ///If the holder is
+                SentViewHolder viewHolder = (SentViewHolder)holder;
+                viewHolder.binding.feeling.setImageResource(reactions[pos]);
+                viewHolder.binding.feeling.setVisibility(View.VISIBLE);
+            } else {
+                ReceiverViewHolder viewHolder = (ReceiverViewHolder)holder;
+                viewHolder.binding.feeling.setImageResource(reactions[pos]);
+                viewHolder.binding.feeling.setVisibility(View.VISIBLE);
+
+
+            }
+
+            message.setFeeling(pos);///pos variable assigns reactions to the message
+
+            FirebaseDatabase.getInstance().getReference()
+                    .child("chats")
+                    .child(senderRoom)
+                    .child("messages")
+                    .child(message.getMessageId()).setValue(message);
+
+            FirebaseDatabase.getInstance().getReference()
+                    .child("chats")
+                    .child(receiverRoom)
+                    .child("messages")
+                    .child(message.getMessageId()).setValue(message);
+
+
+
+            return true; // true is closing popup, false is requesting a new selection
+        });
+
+
+        if(holder.getClass() == SentViewHolder.class) {
+            ///If the holder is SentView type holder, then the holder is typecasted to an object
+            /// of type SentViewHolder
+            SentViewHolder viewHolder = (SentViewHolder)holder;
+
+            if(message.getMessage().equals("photo")) {
+                viewHolder.binding.image.setVisibility(View.VISIBLE);
+                viewHolder.binding.message.setVisibility(View.GONE);
+                Glide.with(context)
+                        .load(message.getImageUrl())
+                        .placeholder(R.drawable.placeholder)
+                        .into(viewHolder.binding.image);
+            }
+
+            viewHolder.binding.message.setText(message.getMessage());
+
+            if(message.getFeeling() >= 0) {
+                ///
+                /// We check if there is a reaction on the message. If there is, then the reaction is set to visible.
+                viewHolder.binding.feeling.setImageResource(reactions[message.getFeeling()]);
+                viewHolder.binding.feeling.setVisibility(View.VISIBLE);
+            } else {
+                viewHolder.binding.feeling.setVisibility(View.GONE);
+            }
+
+            viewHolder.binding.message.setOnTouchListener(new View.OnTouchListener() {
+                /// On touching a message we get the option to react through ReactionPopup()
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    popup.onTouch(v, event);
+                    return false;
+                }
+            });
+
+            viewHolder.binding.image.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    popup.onTouch(v, event);
+                    return false;
+                }
+            });
+
+            viewHolder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    View view = LayoutInflater.from(context).inflate(R.layout.delete_dialog, null);
+                    DeleteDialogBinding binding = DeleteDialogBinding.bind(view);
+                    AlertDialog dialog = new AlertDialog.Builder(context)
+                            .setTitle("Delete Message")
+                            .setView(binding.getRoot())
+                            .create();
+
+                    binding.everyone.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            message.setMessage("This message is removed.");
+                            message.setFeeling(-1);/// We set the reaction as -1 initially(no reaction)
+                            FirebaseDatabase.getInstance().getReference()
+                                    .child("chats")
+                                    .child(senderRoom)
+                                    .child("messages")
+                                    .child(message.getMessageId()).setValue(message);
+
+                            FirebaseDatabase.getInstance().getReference()
+                                    .child("chats")
+                                    .child(receiverRoom)
+                                    .child("messages")
+                                    .child(message.getMessageId()).setValue(message);
+                            dialog.dismiss();
+                        }
+                    });
+
+                    binding.delete.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            FirebaseDatabase.getInstance().getReference()
+                                    .child("chats")
+                                    .child(senderRoom)
+                                    .child("messages")
+                                    .child(message.getMessageId()).setValue(null);
+                            dialog.dismiss();
+                        }
+                    });
+
+                    binding.cancel.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            dialog.dismiss();
+                        }
+                    });
+
+                    dialog.show();
+
+                    return false;
+                }
+            });
+        } else {
+            ReceiverViewHolder viewHolder = (ReceiverViewHolder)holder;
+            if(message.getMessage().equals("photo")) {
+                viewHolder.binding.image.setVisibility(View.VISIBLE);
+                viewHolder.binding.message.setVisibility(View.GONE);
+                Glide.with(context)
+                        .load(message.getImageUrl())
+                        .placeholder(R.drawable.placeholder)
+                        .into(viewHolder.binding.image);
+            }
+            viewHolder.binding.message.setText(message.getMessage());
+
+            if(message.getFeeling() >= 0) {
+                //message.setFeeling(reactions[message.getFeeling()]);
+                viewHolder.binding.feeling.setImageResource(reactions[message.getFeeling()]);
+                viewHolder.binding.feeling.setVisibility(View.VISIBLE);
+            } else {
+                viewHolder.binding.feeling.setVisibility(View.GONE);
+            }
+
+            viewHolder.binding.message.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    popup.onTouch(v, event);
+                    return false;
+                }
+            });
+
+            viewHolder.binding.image.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    popup.onTouch(v, event);
+                    return false;
+                }
+            });
+
+            viewHolder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    View view = LayoutInflater.from(context).inflate(R.layout.delete_dialog, null);
+                    DeleteDialogBinding binding = DeleteDialogBinding.bind(view);
+                    AlertDialog dialog = new AlertDialog.Builder(context)
+                            .setTitle("Delete Message")
+                            .setView(binding.getRoot())
+                            .create();
+
+                    binding.everyone.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            message.setMessage("This message is removed.");
+                            message.setFeeling(-1);
+                            FirebaseDatabase.getInstance().getReference()
+                                    .child("chats")
+                                    .child(senderRoom)
+                                    .child("messages")
+                                    .child(message.getMessageId()).setValue(message);
+
+                            FirebaseDatabase.getInstance().getReference()
+                                    .child("chats")
+                                    .child(receiverRoom)
+                                    .child("messages")
+                                    .child(message.getMessageId()).setValue(message);
+                            dialog.dismiss();
+                        }
+                    });
+
+                    binding.delete.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            FirebaseDatabase.getInstance().getReference()
+                                    .child("chats")
+                                    .child(senderRoom)
+                                    .child("messages")
+                                    .child(message.getMessageId()).setValue(null);
+                            dialog.dismiss();
+                        }
+                    });
+
+                    binding.cancel.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            dialog.dismiss();
+                        }
+                    });
+
+                    dialog.show();
+
+                    return false;
+                }
+            });
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return messages.size();
+    }
+
+    public class SentViewHolder extends RecyclerView.ViewHolder {
+        /// <summary>
+        /// Used for binding the xml file to the Activity backend.
+        /// </summary>
+
+        ItemSentBinding binding;
+        public SentViewHolder(@NonNull View itemView) {
+            super(itemView);
+            binding = ItemSentBinding.bind(itemView);
+        }
+    }
+
+    public class ReceiverViewHolder extends RecyclerView.ViewHolder {
+        /// <summary>
+        /// Used for binding the xml file to the Activity backend.
+        /// </summary>
+
+        ItemReceiveBinding binding;
+
+        public ReceiverViewHolder(@NonNull View itemView) {
+            super(itemView);
+            binding = ItemReceiveBinding.bind(itemView);
+        }
+    }
+
 }
